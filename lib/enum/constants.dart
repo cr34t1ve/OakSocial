af80370f09a38e605a7370922efff1e909a96ac8
cr34t1ve <cr34t1ve@protonmail.com>
2021-05-20 12:56:48 +0000

    added onboarding data to constants.dart

---
+++ lib/enum/constants.dart
@@ -0,0 +1,19 @@
+List<Map<String, String>> pageData = [
+  {
+    "image": "assets/images/Group 1.svg",
+    "text": "Welcome To Oak Social",
+    "subtitle":
+        "The social networking application that\n provides a safe space for families to share\n thoughts, ideas and resources",
+  },
+  {
+    "image": "assets/images/Group 5.svg",
+    "text": "Connect With Your Familly",
+    "subtitle":
+        "Share thoughts, ideas and resources for the\n betterment and upliftment of your family.",
+  },
+  {
+    "image": "assets/images/Group 6.svg",
+    "text": "Create Family Photo\nCollections Together",
+    "subtitle": "Capture and save important moments with\n pictures uploaded to your family Photos "
+  },
+];
