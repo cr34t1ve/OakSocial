ebe5451e59889ce4d53ff2a47606bc3055fe427d
cr34t1ve <cr34t1ve@protonmail.com>
2021-05-20 12:40:32 +0000

    Splash screen constructed

--- lib/splash.dart
+++ lib/splash.dart
@@ -10,7 +10,13 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/OakSocialLogo.png',
+          width: 183.0,
+        )
+      ),
     );
   }
 }
