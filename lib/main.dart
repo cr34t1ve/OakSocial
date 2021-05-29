81e35ddbfde0a03b95c352d0c3917465ccdd5c1d
cr34t1ve <cr34t1ve@protonmail.com>
2021-05-29 18:59:08 +0000

    'sign up' page initialised

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:oak_social/screens/onboarding/onboarding.dart';
 import 'package:oak_social/screens/signin/signin.dart';
+import 'package:oak_social/screens/signup/signup.dart';
 import 'package:oak_social/screens/welcome/welcome.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SignIn(),
+    home: SignUp(),
     debugShowCheckedModeBanner: false,
   ));
 }
