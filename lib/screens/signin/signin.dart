c482bee89bb789005d2514f887f283da6714085f
cr34t1ve <cr34t1ve@protonmail.com>
2021-05-29 18:56:55 +0000

    'Sign In' page constructed

--- lib/screens/signin/signin.dart
+++ lib/screens/signin/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oak_social/components/button.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -6,6 +7,17 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool _isHidden = true;
+
+  void _toggle() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +25,132 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.0,
+        title: Image.asset(
+          'assets/images/OakSocialLogo.png',
+          height: 62,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 70.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome Back',
+                  style: TextStyle(
+                      fontFamily: 'Manrope',
+                      color: Color(0xFF4EAD8B),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 24.0),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Sign In to continue',
+                  style: TextStyle(
+                      fontFamily: 'Manrope',
+                      color: Color(0xFF787879),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14.0),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            SizedBox(
+              width: 334.0,
+              height: 60.0,
+              child: TextField(
+                controller: emailController,
+                autofocus: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              width: 334.0,
+              height: 60.0,
+              child: TextFormField(
+                controller: passwordController,
+                // onSaved: (val) => passwordController.text = val,
+                autofocus: false,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: _isHidden,
+                decoration: InputDecoration(
+                    focusColor: Colors.black,
+                    suffixIcon: GestureDetector(
+                      onTap: _toggle,
+                      child: Icon(
+                        Icons.remove_red_eye_outlined,
+                        //color: Colors.black,
+                      ),
+                    ),
+                    hintText: 'Password',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            DefaultButton(
+              text: 'Sign In',
+              press: () {},
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Need an Account? Join Oak Social',
+                  style: TextStyle(
+                      color: Color(0xFF4299E1),
+                      fontFamily: 'Manrope',
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Forgot Password',
+                  style: TextStyle(
+                      color: Color(0xFF4299E1),
+                      fontFamily: 'Manrope',
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
