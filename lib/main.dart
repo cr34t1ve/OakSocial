d0856ac87d07e90bd85e078339ab11f2388aa0bb
cr34t1ve <cr34t1ve@protonmail.com>
2021-05-30 16:58:01 +0000

    'forgot password' page constructed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oak_social/screens/forgotpassword/forgotpassword.dart';
 import 'package:oak_social/screens/onboarding/onboarding.dart';
 import 'package:oak_social/screens/signin/signin.dart';
 import 'package:oak_social/screens/signup/signup.dart';
@@ -6,7 +7,7 @@ import 'package:oak_social/screens/welcome/welcome.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SignUp(),
+    home: ForgotPassword(),
     debugShowCheckedModeBanner: false,
   ));
 }
--- lib/screens/forgotpassword/forgotpassword.dart
+++ lib/screens/forgotpassword/forgotpassword.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oak_social/components/button.dart';
 
 class ForgotPassword extends StatefulWidget {
   @override
@@ -6,10 +7,86 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
+  TextEditingController _emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Image.asset(
+          'assets/images/OakSocialLogo.png',
+          height: 62,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 70.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Forgot your password ?',
+                  style: TextStyle(
+                      fontFamily: 'Manrope',
+                      color: Color(0xFF4EAD8B),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 24.0),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Enter your email to reset it',
+                  style: TextStyle(
+                      fontFamily: 'Manrope',
+                      color: Color(0xFF787879),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14.0),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            SizedBox(
+              width: 334.0,
+              height: 60.0,
+              child: TextField(
+                controller: _emailController,
+                autofocus: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintStyle: TextStyle(
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14.0),
+                    hintText: 'Email',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            DefaultButton(
+              text: 'Send Link to Reste Password',
+              press: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
