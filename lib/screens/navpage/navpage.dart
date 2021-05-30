2626081784e4485a0f5f02e4b1301bb5ca117f35
cr34t1ve <cr34t1ve@protonmail.com>
2021-05-30 19:36:58 +0000

    bottom navigation bar constructed

--- lib/screens/navpage/navpage.dart
+++ lib/screens/navpage/navpage.dart
@@ -21,6 +21,28 @@ class _NavPageState extends State<NavPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        // unselectedIconTheme: IconThemeData(
+        //   color: Colors.black
+        // ),
+        // selectedIconTheme: IconThemeData(
+        //   color: Colors.pink,
+        //   opacity: 0.5,
+        //   size: 20.0
+        // ),
+        unselectedItemColor: Color(0xFFCAE5DD),
+        selectedItemColor: Colors.white,
+        unselectedLabelStyle: TextStyle(
+          fontFamily: 'Manrope',
+          fontWeight: FontWeight.w600,
+          fontSize: 10.0,
+          // color: Colors.white
+        ),
+        selectedLabelStyle: TextStyle(
+          fontFamily: 'Manrope',
+          fontWeight: FontWeight.w600,
+          fontSize: 10.0,
+          // color: Colors.white
+        ),
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
@@ -37,7 +59,7 @@ class _NavPageState extends State<NavPage> {
               icon: SvgPicture.asset('assets/images/Vector-1.svg'),
               label: 'Explore'),
           BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/images/Vector-3.svg'),
+              icon: SvgPicture.asset('assets/images/clarity_notification-line.svg'),
               label: 'Notification'),
           BottomNavigationBarItem(
               icon: SvgPicture.asset('assets/images/Vector-2.svg'),
